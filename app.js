9d788d52d066683e23456c271539badb56071f4b
abhishekmulik <mulikabhishek2606@gmail.com>
2020-12-24 12:00:37 +0530

    adding new item to UI

--- app.js
+++ app.js
@@ -64,7 +64,9 @@ var UIController=(function(){
         inputType:'.add__type',
         inputDescription:'.add__description',
         inputValue: '.add__value',
-        inputBtn:'.add__btn'
+        inputBtn:'.add__btn',
+        incomeContainer:'.income__list',
+        expensesContainer:'.expenses__list'
     };
 
     return {
@@ -75,6 +77,26 @@ var UIController=(function(){
           value : document.querySelector(DOMstrings.inputValue).value,
           };
       }, 
+
+      addListItem: function(obj,type){
+          var html,newHtml,element; 
+        //create html string with placeholer text
+        if (type==='inc'){
+            element=DOMstrings.incomeContainer;
+            html='<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        }else if(type==='exp'){
+            element=DOMstrings.expensesContainer;
+            html='<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+        }
+        
+        //replace the placeholder text with some actual data
+        newHtml=html.replace('%id%',obj.id);
+        newHtml=newHtml.replace('%description%',obj.description);
+        newHtml=newHtml.replace('%value%',obj.value);
+
+        //insert html in dom
+        document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
+      },
       getDomstrings: function(){
           return DOMstrings;
       }
@@ -104,7 +126,9 @@ var controller=(function(budgetCtrl,UICtrl){
         
         //2 add the item to budget to budget controller
         newItem=budgetCtrl.addItem(input.type,input.description,input.value);
+        
         //3. add the item to UI 
+        UICtrl.addListItem(newItem,input.type); 
 
         //4. calc the budegt
 
